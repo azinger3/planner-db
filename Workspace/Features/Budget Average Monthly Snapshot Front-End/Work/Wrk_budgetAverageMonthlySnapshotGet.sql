23258379386c96f422a511795fb00acefd345226
Rob Azinger <razinger@techstyle.com>
2019-12-31 00:29:35 -0500

    Initial SP

---
+++ Workspace/Features/Budget Average Monthly Snapshot Front-End/Work/Wrk_budgetAverageMonthlySnapshotGet.sql
@@ -0,0 +1,16 @@
+USE `planner`;
+
+
+
+/********************************************************************************************** 
+PURPOSE:		Get list of Budget Average Monthly Snapshots
+AUTHOR:			Rob Azinger
+DATE:			12/31/2019
+NOTES:			Snapshot Table - snpBudgetAverageMonthly
+CHANGE CONTROL:		 
+***********************************************************************************************/
+
+-- BudgetAverageMonthlySnapshotGet
+
+SELECT 	* 
+FROM 	snpBudgetAverageMonthly;
