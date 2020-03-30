773968f036d6c36db915dccd10f2ee6f0db55f61
Rob Azinger <razinger@techstyle.com>
2020-03-30 15:38:59 -0400

    updates

--- Lookups/luBudgetCategory.sql
+++ Lookups/luBudgetCategory.sql
@@ -1,11 +1,16 @@
-select * from BudgetCategory where BudgetCategoryID in (78,79);
-select * from BudgetItem where BudgetCategoryID in (76);
+select * from BudgetCategory where ActiveFlg = 1 and BudgetCategoryID in (78,79);
+select * from BudgetItem where ActiveFlg = 1 and  BudgetCategoryID in (76);
+select * from Fund where ActiveFlg = 1;
 -- select * from TransactionType;
 
 /*
 update BudgetCategory
 set BudgetCategory = 'Pets'
 where BudgetCategoryID = 88;
+
+update Fund
+set FundName = 'Car Care Fund'
+where FundID = 15;
 
 delete from BudgetCategory
 where BudgetCategoryID in (76,79);
