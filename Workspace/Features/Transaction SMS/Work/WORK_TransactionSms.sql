51dcb32f614ecd94891e13b0c4a9548962b0e603
Rob Azinger <razinger@techstyle.com>
2020-07-21 17:47:43 -0400

    Updates it works!

--- Workspace/Features/Transaction SMS/Work/WORK_TransactionSms.sql
+++ Workspace/Features/Transaction SMS/Work/WORK_TransactionSms.sql
@@ -23,7 +23,24 @@ CREATE TABLE TransactionSms
 (
 	TransactionSmsID		INT(10) NOT NULL AUTO_INCREMENT
 	,Sender						VARCHAR(100)
+    ,Receiver 					VARCHAR(100)
     ,Body							VARCHAR(1000)
+    ,SmsSid 						VARCHAR(1000)
+	,SmsMessageSid 		VARCHAR(1000)
+    ,SmsStatus 					VARCHAR(100)
+    ,AccountSid 				VARCHAR(1000)
+    ,MessageSid 				VARCHAR(1000)
+    ,FromCity 					VARCHAR(100)
+	,FromState 					VARCHAR(100)
+	,FromZip 						VARCHAR(100)
+	,FromCountry 				VARCHAR(100)
+	,ToState 						VARCHAR(100)
+    ,ToCity 						VARCHAR(100)
+    ,ToZip 							VARCHAR(100)
+    ,ToCountry 					VARCHAR(100)
+	,NumMedia 					VARCHAR(100)
+    ,NumSegments 			VARCHAR(100)
+	,ApiVersion 					VARCHAR(100)
 	,CreateDT      				DATETIME DEFAULT(NOW())
 	,CreateBy      				VARCHAR(100)
 	,ModifyDT					DATETIME
@@ -41,7 +58,24 @@ CREATE TABLE logTransactionSms
 	logTransactionSmsID	INT(10) NOT NULL AUTO_INCREMENT
     ,TransactionSmsID		INT(10)
 	,Sender						VARCHAR(100)
+    ,Receiver 					VARCHAR(100)
     ,Body							VARCHAR(1000)
+    ,SmsSid 						VARCHAR(1000)
+	,SmsMessageSid 		VARCHAR(1000)
+    ,SmsStatus 					VARCHAR(100)
+    ,AccountSid 				VARCHAR(1000)
+    ,MessageSid 				VARCHAR(1000)
+    ,FromCity 					VARCHAR(100)
+	,FromState 					VARCHAR(100)
+	,FromZip 						VARCHAR(100)
+	,FromCountry 				VARCHAR(100)
+	,ToState 						VARCHAR(100)
+    ,ToCity 						VARCHAR(100)
+    ,ToZip 							VARCHAR(100)
+    ,ToCountry 					VARCHAR(100)
+	,NumMedia 					VARCHAR(100)
+    ,NumSegments 			VARCHAR(100)
+	,ApiVersion 					VARCHAR(100)
 	,CreateDT      				DATETIME DEFAULT(NOW())
 	,CreateBy      				VARCHAR(100)
 	,ModifyDT					DATETIME
@@ -93,3 +127,28 @@ SELECT  999 AS TransactionSmsID
 select * from TransactionSms order by 1 desc limit 5;
 select * from logTransactionSms order by 1 desc limit 5;
 
+
+
+/*
+
+ToCountry = US
+ToState = DC
+SmsMessageSid = SMd82d5b77c303c4150174c9f07fceedfe
+NumMedia = 0
+ToCity
+FromZip = 40359
+SmsSid=SMd82d5b77c303c4150174c9f07fceedfe
+FromState=KY
+SmsStatus=received
+FromCity=LOUISVILLE
+Body=Hello \xf0\x9f\x91\x8b
+FromCountry = US
+To = +12029722265
+ToZip = 
+NumSegments = 1
+MessageSid = SMd82d5b77c303c4150174c9f07fceedfe
+AccountSid = AC49e7e50cf672d78967f723afbd982e20
+From = +15029302955
+ApiVersion = 2010-04-01
+
+*/
--- Workspace/Features/Transaction SMS/Work/WORK_TransactionSmsInsert.sql
+++ Workspace/Features/Transaction SMS/Work/WORK_TransactionSmsInsert.sql
@@ -5,23 +5,91 @@ DROP PROCEDURE IF EXISTS `TransactionSmsInsert`;
 DELIMITER ;;
 CREATE PROCEDURE `TransactionSmsInsert` 
 (
-	prmSender VARCHAR(100)
-	,prmBody VARCHAR(1000)
+    prmSender						VARCHAR(100)
+    ,prmReceiver 					VARCHAR(100)
+    ,prmBody							VARCHAR(1000)
+    ,prmSmsSid 					VARCHAR(1000)
+	,prmSmsMessageSid 		VARCHAR(1000)
+    ,prmSmsStatus 				VARCHAR(100)
+    ,prmAccountSid 				VARCHAR(1000)
+    ,prmMessageSid 			VARCHAR(1000)
+    ,prmFromCity 					VARCHAR(100)
+	,prmFromState 				VARCHAR(100)
+	,prmFromZip 					VARCHAR(100)
+	,prmFromCountry 			VARCHAR(100)
+	,prmToState 					VARCHAR(100)
+    ,prmToCity 						VARCHAR(100)
+    ,prmToZip 						VARCHAR(100)
+    ,prmToCountry 				VARCHAR(100)
+	,prmNumMedia 				VARCHAR(100)
+    ,prmNumSegments 		VARCHAR(100)
+	,prmApiVersion 				VARCHAR(100)
 )
 BEGIN
 
 SET @varSender = prmSender;
+SET @varReceiver = prmReceiver;
 SET @varBody = prmBody;
+SET @varSmsSid = prmSmsSid;
+SET @varSmsMessageSid = prmSmsMessageSid;
+SET @varSmsStatus = prmSmsStatus;
+SET @varAccountSid = prmAccountSid;
+SET @varMessageSid = prmMessageSid;
+SET @varFromCity = prmFromCity;
+SET @varFromState = prmFromState;
+SET @varFromZip = prmFromZip;
+SET @varFromCountry = prmFromCountry;
+SET @varToState = prmToState;
+SET @varToCity = prmToCity;
+SET @varToZip = prmToZip;
+SET @varToCountry = prmToCountry;
+SET @varNumMedia = prmNumMedia;
+SET @varNumSegments = prmNumSegments;
+SET @varApiVersion = prmApiVersion;
 
 INSERT INTO TransactionSms
 (
-	Sender					
+	Sender		
+    ,Receiver
 	,Body
+	,SmsSid
+	,SmsMessageSid
+	,SmsStatus
+	,AccountSid
+	,MessageSid
+	,FromCity
+	,FromState
+	,FromZip
+	,FromCountry
+	,ToState
+	,ToCity
+	,ToZip
+	,ToCountry
+	,NumMedia
+	,NumSegments
+	,ApiVersion
 	,CreateBy
 )
-SELECT  @varSender	AS Sender						
-				,@varBody		AS Body
-				,'Bot'				AS CreateBy
+SELECT  @varSender					AS Sender
+				,@varReceiver				AS Receiver						
+				,@varBody						AS Body
+				,@varSmsSid 					AS SmsSid
+				,@varSmsMessageSid 	AS SmsMessageSid
+				,@varSmsStatus 			AS SmsStatus
+				,@varAccountSid 			AS AccountSid
+				,@varMessageSid 			AS MessageSid
+				,@varFromCity 				AS FromCity
+				,@varFromState 			AS FromState
+				,@varFromZip 				AS FromZip
+				,@varFromCountry 		AS FromCountry
+				,@varToState 					AS ToState
+				,@varToCity 					AS ToCity
+				,@varToZip 					AS ToZip
+				,@varToCountry 				AS ToCountry
+				,@varNumMedia 			AS NumMedia
+				,@varNumSegments 		AS NumSegments
+				,@varApiVersion 			AS ApiVersion
+				,'Bot'								AS CreateBy
 ;
 
 END;;
